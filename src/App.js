552a4fe3c74d51b1c1e42e3d78a64866389256ba
agagotowiec <92727370+agagotowiec@users.noreply.github.com>
2022-02-03 20:43:28 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,25 @@ import Weather from "./Weather";
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
+			<div className="container">
+				<h1>Weather App</h1>
+
 				<Weather />
-			</header>
+				<footer>
+					This page is {""}
+					<a
+						href="https://github.com/agagotowiec/react-weather-app"
+						target="_blank"
+						rel="noreferrer"
+					>
+						open-sourced.{" "}
+					</a>
+					It was coded by {""}
+					<a href="https://www.linkedin.com/in/aga-got/" target="_blank" rel="noreferrer">
+						Aga Gotowiec.
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
