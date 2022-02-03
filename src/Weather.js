4c94efefca0041ab67242d379abf5cd8dc718a89
agagotowiec <92727370+agagotowiec@users.noreply.github.com>
2022-02-03 18:39:37 +0000

    Added React weather app wireframe

---
+++ src/Weather.js
@@ -0,0 +1,63 @@
+import React from "react";
+import axios from "axios";
+
+import "./App.css";
+import "./Weather.css";
+
+export default function Weather() {
+	let weatherData = {
+		city: "Rome",
+		temperature: 20,
+		date: "Wednesday, 19:26",
+		description: "Cloudy",
+		imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+		humidity: 20,
+		wind: 14,
+	};
+
+	return (
+		<div className="Weather">
+			<form className="mb-3">
+				<div className="row">
+					<div className="col-6">
+						<input
+							type="search"
+							placeholder="Type a city.."
+							className="form-control"
+							autoComplete="off"
+						/>
+					</div>
+					<div className="col-6">
+						<input type="submit" value="Search" className="btn btn-primary w-200" />
+					</div>
+				</div>
+			</form>
+			<div className="overview">
+				<h1>{weatherData.city}</h1>
+				<ul>
+					<li>{weatherData.date}</li>
+					<li>{weatherData.description}</li>
+				</ul>
+			</div>
+			<div className="row">
+				<div className="col-6">
+					<div className="clearfix weather-temperature">
+						<img src={weatherData.imgUrl} alt={weatherData.description} className="float-left" />
+						<div className="float-left">
+							<strong>{weatherData.temperature}</strong>
+							<span className="units">
+								<a href="/">°C</a> | <a href="/">°F</a>
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="col-6">
+					<ul>
+						<li>Humidity: {weatherData.humidity}%</li>
+						<li>Wind: {weatherData.wind} km/h</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+}
